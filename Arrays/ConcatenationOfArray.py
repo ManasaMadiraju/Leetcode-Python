1b94e4697e2de23989ae8301f9c751d2970d5d43
ManasaMadiraju <manasamadiraju343@gmail.com>
2024-07-23 00:50:16 -0700

    Leetcode 1929

---
+++ Arrays/ConcatenationOfArray.py
@@ -0,0 +1,18 @@
+class Solution:
+    def getConcatenation(self, nums: list[int]) -> list[int]:
+        ans = []
+
+        for i in range (2):
+            for n in nums:
+                ans.append(n)
+        return ans
+    
+def main():
+    nums = [1,2,1]
+    s = Solution()
+    result = s.getConcatenation(nums)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
