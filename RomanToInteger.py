7bd88a54b17ce6c2589f3ccd975f68d3b68e7f8d
ManasaMadiraju <manasamadiraju343@gmail.com>
2024-07-29 08:55:55 -0700

    Leetcode 13

---
+++ RomanToInteger.py
@@ -0,0 +1,19 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        romantoint={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        res=0
+        for i in range(len(s)):
+            if s[i] in romantoint: # if 
+                res+=romantoint[s[i]]
+        return res
+    
+
+def main():
+    s = "MCMXCIV"
+    sol = Solution()
+    result = sol.romanToInt(s)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
